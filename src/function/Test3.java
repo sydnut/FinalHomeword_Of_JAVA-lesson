829083b4ab97a5fe608a1bf38a23ef7965a4748a
sydnut <3137400500@qq.com>
2024-11-07 11:54:03 +0800

    first update

--- src/function/Test3.java
+++ src/function/Test3.java
@@ -15,7 +15,7 @@ import java.util.Scanner;
  * @time 2024/11/1
  */
 public class Test3 {
-    private String learned_Vocabulury = "C:\\Users\\31374\\IdeaProjects\\FinalHomework\\src\\vocabulary\\";
+    public String learned_Vocabulury = "C:\\Users\\31374\\IdeaProjects\\FinalHomework\\src\\vocabulary\\";
     private String unknown_Vocabulury = "C:\\Users\\31374\\IdeaProjects\\FinalHomework\\src\\vocabulary\\";
     private HashMap<String, String> BufferedMap_1 = new HashMap<>();
     private HashMap<String, String> BufferedMap_2 = new HashMap<>();
@@ -37,6 +37,16 @@ public class Test3 {
         learned_Vocabulury += (id + "_learned.txt");
         unknown_Vocabulury += (id + "_unknown.txt");
     }
+
+    public static void main(String[] args) throws IOException {
+        Test3 v = new Test3("trial");
+        try (RandomAccessFile file = new RandomAccessFile(v.learned_Vocabulury, "rw")) {
+            file.write("234\r\n".getBytes(StandardCharsets.UTF_8));
+            file.write("asdddwn\r\n".getBytes(StandardCharsets.UTF_8));
+
+        }
+    }
+
     /*
     public static void main(String[] args) throws IOException {
         Test3 test = new Test3("1");
@@ -53,6 +63,7 @@ public class Test3 {
     public void addUnknown(String en, String cn) {
         BufferedMap_2.put(en, cn);
     }
+
     //将缓冲区（本次使用软件产生的词汇）输出到文件中
     public void outputVocabulary() {
         //rw，如果不存在文件则创建文件
@@ -60,10 +71,10 @@ public class Test3 {
             randomAccessFile.seek(randomAccessFile.length());
             BufferedMap_1.forEach((a, b) -> {
                 try {
-                    randomAccessFile.writeUTF(a);
-                    randomAccessFile.write(System.lineSeparator().getBytes(StandardCharsets.UTF_8));
-                    randomAccessFile.writeUTF(b);
-                    randomAccessFile.write(System.lineSeparator().getBytes(StandardCharsets.UTF_8));
+                    randomAccessFile.write(a.getBytes(StandardCharsets.UTF_8));
+                    randomAccessFile.write("\r\n".getBytes(StandardCharsets.UTF_8));
+                    randomAccessFile.write(b.getBytes(StandardCharsets.UTF_8));
+                    randomAccessFile.write("\r\n".getBytes(StandardCharsets.UTF_8));
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -76,10 +87,10 @@ public class Test3 {
             randomAccessFile.seek(randomAccessFile.length());
             BufferedMap_2.forEach((a, b) -> {
                 try {
-                    randomAccessFile.writeUTF(a);
-                    randomAccessFile.write(System.lineSeparator().getBytes(StandardCharsets.UTF_8));
-                    randomAccessFile.writeUTF(b);
-                    randomAccessFile.write(System.lineSeparator().getBytes(StandardCharsets.UTF_8));
+                    randomAccessFile.write(a.getBytes(StandardCharsets.UTF_8));
+                    randomAccessFile.write("\r\n".getBytes(StandardCharsets.UTF_8));
+                    randomAccessFile.write(b.getBytes(StandardCharsets.UTF_8));
+                    randomAccessFile.write("\r\n".getBytes(StandardCharsets.UTF_8));
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -93,19 +104,22 @@ public class Test3 {
         BufferedMap_1.clear();
         BufferedMap_2.clear();
     }
+
     //从文件中读出单词
     public void checkVocabulary() {
         JFrame frame = new JFrame("vocabulary");
         frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 
-        frame.setBounds(500,200,600, 500);
+        frame.setBounds(500, 200, 600, 500);
         JScrollPane jScrollPane = new JScrollPane();
         JPanel jPanel = new JPanel();
         jPanel.setLayout(new BoxLayout(jPanel, BoxLayout.Y_AXIS));
 
         try (BufferedReader scanner = new BufferedReader(new InputStreamReader(new FileInputStream(learned_Vocabulury)))) {
-            jPanel.add(new JLabel("Learned vocabulary:"));
-
+            JPanel rowPanel1 = new JPanel();
+            rowPanel1.setLayout(new FlowLayout(FlowLayout.LEFT)); // 水平排列
+            rowPanel1.add((new JLabel("Learned vocabulary:")));
+            jPanel.add(rowPanel1);
             while (scanner.ready()) {
                 String a = scanner.readLine();
                 String b = scanner.readLine();
@@ -120,7 +134,10 @@ public class Test3 {
             e.printStackTrace();
         }
         try (BufferedReader scanner = new BufferedReader(new InputStreamReader(new FileInputStream(unknown_Vocabulury)))) {
-            jPanel.add(new JLabel("Unknown vocabulary:"));
+            JPanel rowPanel1 = new JPanel();
+            rowPanel1.setLayout(new FlowLayout(FlowLayout.LEFT)); // 水平排列
+            rowPanel1.add((new JLabel("Unknown vocabulary:")));
+            jPanel.add(rowPanel1);
             while (scanner.ready()) {
                 String a = scanner.readLine();
                 String b = scanner.readLine();
